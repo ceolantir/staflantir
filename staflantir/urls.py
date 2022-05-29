bf0d2489ff9e647618f541eb1b4e8f97406b8669
ceolantir <songhoyeon16@gmail.com>
2022-05-29 21:13:23 +0700

    Base directory

---
+++ staflantir/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from palantir.views import page_not_found, server_error
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('palantir.urls')),
+]
+
+handler404 = page_not_found
+handler500 = server_error
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
