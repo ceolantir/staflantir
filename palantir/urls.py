a11709b9d19a5ed9287ac577caaf77fb6005e099
ceolantir <songhoyeon16@gmail.com>
2022-06-03 05:08:02 +0700

    added a page for changing information about a specialist

--- palantir/urls.py
+++ palantir/urls.py
@@ -8,6 +8,7 @@ from .views import (
     SearchView,
     ChoiceInformationSourcesView,
     ApplicationView,
+    DetailSpecialistEditView,
 )
 
 urlpatterns = [
@@ -15,6 +16,7 @@ urlpatterns = [
     path('information_sources/<int:pk>/', DetailInformationSourceView.as_view(), name='detail_information_source'),
     path('specialists/', SpecialistsView.as_view(), name='specialists'),
     path('specialists/<int:pk>/', DetailSpecialistView.as_view(), name='detail_specialist'),
+    path('specialists/<int:pk>/edit', DetailSpecialistEditView.as_view(), name='detail_specialist_edit'),
     path('search/', SearchView.as_view(), name='search'),
     path('choice_information_sources/', ChoiceInformationSourcesView.as_view(), name='choice_information_sources'),
     path('application/<str:information_sources>/', ApplicationView.as_view(), name='application'),
