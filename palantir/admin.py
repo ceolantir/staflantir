6a7742ccd5927c00de1b877ac5e211417ad5ef77
ceolantir <songhoyeon16@gmail.com>
2022-06-04 04:33:33 +0700

    * Added presentation of information about the specialist's phone for the admin panel

--- palantir/admin.py
+++ palantir/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import InformationSource, Specialist, VKDataSpecialist
+from .models import (
+    InformationSource,
+    Specialist,
+    VKDataSpecialist,
+    PhoneNumberInformationSpecialist,
+)
 
 
 @admin.register(InformationSource)
@@ -32,4 +37,15 @@ class VKDataSpecialistAdmin(admin.ModelAdmin):
         'last_name',
     )
     list_display_links = ('specialist', 'first_name', 'last_name',)
+    list_filter = ('specialist', 'specialist__owner',)
+
+
+@admin.register(PhoneNumberInformationSpecialist)
+class PhoneNumberInformationSpecialistAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'specialist',
+        'phone'
+    )
+    list_display_links = ('specialist', 'phone',)
     list_filter = ('specialist', 'specialist__owner',)
