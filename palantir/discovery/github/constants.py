4e8096fbbd4b164fe22b6681539947e78008d51a
ceolantir <songhoyeon16@gmail.com>
2022-06-05 00:45:48 +0700

    * Added business logic constants for data from github

---
+++ palantir/discovery/github/constants.py
@@ -0,0 +1,29 @@
+attributes_for_profile_info = (
+    'name',
+    'public_repos',
+    'followers',
+    'following',
+    'company',
+    'blog',
+    'location',
+    'email',
+    'bio',
+    'twitter_username',
+    'created_at',
+    'updated_at',
+)
+attributes_for_repos_info = (
+    'name',
+    'language',
+    'visibility',
+    'archived',
+    'fork',
+    'created_at',
+    'updated_at',
+    'stargazers_count',
+    'forks',
+)
+attributes_for_contributors_info = (
+    'login',
+    'contributions',
+)
