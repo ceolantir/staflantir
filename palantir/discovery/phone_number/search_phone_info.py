9c98cb97d693a01bffcc75a6fc1d8e5a2cbb0260
ceolantir <songhoyeon16@gmail.com>
2022-06-05 03:27:07 +0700

    * The module has been modified to get detailed information about the phone

--- palantir/discovery/phone_number/search_phone_info.py
+++ palantir/discovery/phone_number/search_phone_info.py
@@ -1,36 +1,62 @@
-import urllib.request
-import json
-from typing import NamedTuple
+from typing import Dict, Any
+
+import requests
 
 from palantir.discovery.phone_number import exceptions
+from palantir.models import PhoneNumberInfo
 
 
-class PhoneInfo(NamedTuple):
-    country: str
-    okrug: str
-    region: str
-    time_zone: str
-    oper_brand: str
+class PhoneNumber:
+    def __init__(self):
+        self._base_url = 'https://htmlweb.ru/geo/api.php?json&telcod='
 
+    def __call__(self, phone: str, specialist) -> None:
+        phone_number_info = self._search_phone(phone)
 
-def search_phone(phone: str) -> PhoneInfo:
-    url = "https://htmlweb.ru/geo/api.php?json&telcod=" + phone
-    try:
-        phone_info = urllib.request.urlopen(url)
-        phone_info_json = json.load(phone_info)
+        self._record_info(specialist, phone_number_info, int(phone))
 
-        return PhoneInfo(
-            phone_info_json['fullname'],
-            phone_info_json['okrug'],
-            phone_info_json['region']['name'],
-            phone_info_json['tz'],
-            phone_info_json['0']['oper_brand'],
-        )
-    except Exception as e:
-        raise exceptions.PhoneError(f'PhoneError')
+    def _search_phone(self, phone: str) -> Dict[str, Any]:
+        url = self._base_url + phone
 
+        try:
+            response = requests.get(url).json()
 
-# if __name__ == "__main__":
-#     phone_info = search_phone('79528813602')
-#     for info in phone_info:
-#         print(f'{info}')
+            return self._get_info_from_response(response)
+        except Exception as e:
+            raise exceptions.PhoneError(f'PhoneError')
+
+    @staticmethod
+    def _get_info_from_response(response: Dict[str, Any]) -> Dict[str, Any]:
+        github_data_specialist = {}
+        for key, value in response.items():
+            if key == 'fullname':
+                github_data_specialist['country'] = value
+                continue
+            elif key == 'okrug':
+                github_data_specialist['okrug'] = value
+                continue
+            elif key == 'region':
+                github_data_specialist[key] = value['name']
+                continue
+            elif key == 'tz':
+                github_data_specialist['time_zone'] = value
+                continue
+            elif key == '0':
+                github_data_specialist['oper_brand'] = value['oper_brand']
+                continue
+
+        return github_data_specialist
+
+    @staticmethod
+    def _record_info(
+        specialist,
+        phone_number_info: Dict[str, Any],
+        phone: int,
+    ) -> None:
+        phone_number_specialist_info = PhoneNumberInfo.objects.update_or_create(
+            specialist=specialist,
+            phone=phone,
+        )[0]
+        for key, value in phone_number_info.items():
+            setattr(phone_number_specialist_info, key, value)
+        phone_number_specialist_info.save()
