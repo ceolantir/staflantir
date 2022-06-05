9a12814072ff69af2dcfa10d42860c4925043dd3
ceolantir <songhoyeon16@gmail.com>
2022-06-05 19:08:02 +0700

    * Minor changes

--- staflantir/urls.py
+++ staflantir/urls.py
@@ -15,5 +15,5 @@ handler404 = page_not_found
 handler500 = server_error
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
